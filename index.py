1e4285fcdfef305ae231942490657dcc2ad79cd9
sherali906 <muhammad2144089@gmail.com>
2024-10-01 15:06:35 +0500

    practice-python1

---
+++ index.py
@@ -0,0 +1,18 @@
+mix = ("variable") # mix: list
+print(mix[0])
+print(mix[1])
+print(mix[2])
+mix = [ "bcunties","utencids", 3.1,  41]
+print(mix[0])
+print(mix[1])
+print(mix[2])
+print(mix[3])
+mix = [ "bcunties", "utencids", 3.1, 41]
+print(mix[-1])
+print(mix[-2])
+print(mix[-3])
+print(mix[-4])
+mix = [ "test", 1.4, 5] # mix: list
+print(mix[0])
+print(mix[1])
+print(mix[2])
